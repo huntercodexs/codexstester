c786ff52c60806bcd3b3d6e851ea0ff8e22ede1a
Jereelton Teixeira <jereeltondevel@gmail.com>
2023-03-25 12:38:17 -0300

    update: [stabled] [done] Added simple oauth2Token methods to get token

--- test/external/SampleExternalTests.java
+++ test/external/SampleExternalTests.java
@@ -9,6 +9,13 @@ import org.springframework.http.MediaType;
 
 public class SampleExternalTests extends SampleBridgeTests {
 
+    public String oauth2Token() {
+        Oauth2RequestTokenDto oauth2RequestTokenDto = codexsTesterSecurityOAuth2Token();
+        ResponseEntity<Oauth2ResponseTokenDto> response = codexsTesterExternalOAuth2GetToken(oauth2RequestTokenDto);
+        if (response.getBody() != null) return response.getBody().getAccess_token();
+        return null;
+    }
+
     /**
      * ExternalSampleTests Helpers
      * THIS TESTS CAN BE REMOVED
