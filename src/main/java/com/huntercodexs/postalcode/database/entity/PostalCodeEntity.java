703fa7074f0248b35be412492c18dd058ae75773
Hunter Codexs <huntercodexs@gmail.com>
2024-09-29 21:45:29 -0300

    feat: [stabled] [work] Return to env devel using java 17

--- src/main/java/com/huntercodexs/postalcode/database/entity/PostalCodeEntity.java
+++ src/main/java/com/huntercodexs/postalcode/database/entity/PostalCodeEntity.java
@@ -1,6 +1,6 @@
 package com.huntercodexs.postalcode.database.entity;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
