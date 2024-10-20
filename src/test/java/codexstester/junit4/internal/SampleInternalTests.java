01ccf6f0f9a88a6ccd7d432165b3b50eadff4d2a
Hunter Codexs <huntercodexs@gmail.com>
2024-10-19 22:33:13 -0300

    feat: [stable] [done] [tested] Added crud sample in the sample controller + Fixed bugs in the internal requester

--- src/test/java/codexstester/junit4/internal/SampleInternalTests.java
+++ src/test/java/codexstester/junit4/internal/SampleInternalTests.java
@@ -4,8 +4,13 @@ import codexstester.setup.bridge.SampleBridgeTest;
 import codexstester.setup.datasource.SampleDataSource;
 import com.huntercodexs.codexstester.dto.HeadersDto;
 import com.huntercodexs.codexstester.dto.RequestDto;
+import net.minidev.json.JSONObject;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import static codexstester.setup.datasource.SampleDataSource.*;
 
 public class SampleInternalTests extends SampleBridgeTest {
 
@@ -68,6 +73,7 @@ public class SampleInternalTests extends SampleBridgeTest {
 
     /*SAMPLE*/
     @Test
+    @Ignore
     public void whenAnyOkRequestSample_WithNoAuth_RetrieveCreated_StatusCode201_ByHttpMethodPOST() throws Exception {
         HeadersDto headersDto = new HeadersDto();
         headersDto.setContentType(MediaType.APPLICATION_JSON_VALUE);
@@ -80,6 +86,111 @@ public class SampleInternalTests extends SampleBridgeTest {
         requestDto.setExpectedMessage(null);
 
         codexsTesterInternal_StatusCode201_RetrieveCreated(headersDto, requestDto);
+    }
+
+    /*SAMPLE - GET*/
+    @Test
+    public void whenRequestTest_RetrieveOK_StatusCode200_ByHttpMethodGET() throws Exception {
+        JSONObject dataRequest = new JSONObject();
+
+        HeadersDto headersDto = new HeadersDto();
+        headersDto.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        headersDto.setHttpMethod(HTTP_METHOD_GET);
+        headersDto.setAuthorizationBasic("123-TEST");
+
+        RequestDto requestDto = new RequestDto();
+        requestDto.setExpectedCode(OK_200);
+        requestDto.setUri(endpointGet);
+        requestDto.setId("123456");
+        requestDto.setDataRequest(dataRequest.toString());
+        requestDto.setExpectedMessage(null);
+
+        MockHttpServletResponse dispatcherResult = codexsTesterInternalDispatcher(requestDto, headersDto);
+        codexsTesterAssertExact("GET OK - 123456", dispatcherResult.getContentAsString(), null);
+    }
+
+    /*SAMPLE - POST*/
+    @Test
+    public void whenRequestTest_RetrieveCreated_StatusCode201_ByHttpMethodPOST() throws Exception {
+        JSONObject dataRequest = new JSONObject();
+
+        HeadersDto headersDto = new HeadersDto();
+        headersDto.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        headersDto.setHttpMethod(HTTP_METHOD_POST);
+        headersDto.setAuthorizationBasic("123-TEST");
+
+        RequestDto requestDto = new RequestDto();
+        requestDto.setExpectedCode(CREATED_201);
+        requestDto.setUri(endpointPost);
+        requestDto.setId("123456");
+        requestDto.setDataRequest(dataRequest.toString());
+        requestDto.setExpectedMessage(null);
+
+        MockHttpServletResponse dispatcherResult = codexsTesterInternalDispatcher(requestDto, headersDto);
+        codexsTesterAssertExact("POST OK - 123456", dispatcherResult.getContentAsString(), null);
+    }
+
+    /*SAMPLE - PUT*/
+    @Test
+    public void whenRequestTest_RetrieveAccepted_StatusCode202_ByHttpMethodPUT() throws Exception {
+        JSONObject dataRequest = new JSONObject();
+
+        HeadersDto headersDto = new HeadersDto();
+        headersDto.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        headersDto.setHttpMethod(HTTP_METHOD_PUT);
+        headersDto.setAuthorizationBasic("123-TEST");
+
+        RequestDto requestDto = new RequestDto();
+        requestDto.setExpectedCode(ACCEPTED_202);
+        requestDto.setUri(endpointPut);
+        requestDto.setId("123456");
+        requestDto.setDataRequest(dataRequest.toString());
+        requestDto.setExpectedMessage(null);
+
+        MockHttpServletResponse dispatcherResult = codexsTesterInternalDispatcher(requestDto, headersDto);
+        codexsTesterAssertExact("PUT OK - 123456", dispatcherResult.getContentAsString(), null);
+    }
+
+    /*SAMPLE - DELETE*/
+    @Test
+    public void whenRequestTest_RetrieveAccepted_StatusCode202_ByHttpMethodDELETE() throws Exception {
+        JSONObject dataRequest = new JSONObject();
+
+        HeadersDto headersDto = new HeadersDto();
+        headersDto.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        headersDto.setHttpMethod(HTTP_METHOD_DELETE);
+        headersDto.setAuthorizationBasic("123-TEST");
+
+        RequestDto requestDto = new RequestDto();
+        requestDto.setExpectedCode(ACCEPTED_202);
+        requestDto.setUri(endpointDelete);
+        requestDto.setId("123456");
+        requestDto.setDataRequest(dataRequest.toString());
+        requestDto.setExpectedMessage(null);
+
+        MockHttpServletResponse dispatcherResult = codexsTesterInternalDispatcher(requestDto, headersDto);
+        codexsTesterAssertExact("DELETE OK - 123456", dispatcherResult.getContentAsString(), null);
+    }
+
+    /*SAMPLE - PATCH*/
+    @Test
+    public void whenRequestTest_RetrieveAccepted_StatusCode202_ByHttpMethodPATCH() throws Exception {
+        JSONObject dataRequest = new JSONObject();
+
+        HeadersDto headersDto = new HeadersDto();
+        headersDto.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        headersDto.setHttpMethod(HTTP_METHOD_PATCH);
+        headersDto.setAuthorizationBasic("123-TEST");
+
+        RequestDto requestDto = new RequestDto();
+        requestDto.setExpectedCode(ACCEPTED_202);
+        requestDto.setUri(endpointPatch);
+        requestDto.setId("123456");
+        requestDto.setDataRequest(dataRequest.toString());
+        requestDto.setExpectedMessage(null);
+
+        MockHttpServletResponse dispatcherResult = codexsTesterInternalDispatcher(requestDto, headersDto);
+        codexsTesterAssertExact("PATCH OK - 123456", dispatcherResult.getContentAsString(), null);
     }
 
 }
